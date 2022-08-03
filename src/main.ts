ef907d14394aa3baafe1c1f7f48eedf7d4f926cc
RLazamh <roger.laza@epn.edu.ec>
2022-08-02 22:41:39 -0500

    add concepts of high and low coupling

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import './style.css'
-import './01-names/07-task'
+// import './01-names/07-task'
 // import './01-names/05-dry'
+// import './02-code-smell/02-high-coupling'
+import './02-code-smell/02-low-coupling'
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
