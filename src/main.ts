04691caf15fb8207d61b0515e88ba09889d06c6a
RLazamh <roger.laza@epn.edu.ec>
2022-08-05 18:09:50 -0500

    add dependency inverse concepts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import './style.css'
 // import './01-names/05-dry'
 // import './02-code-smell/02-low-coupling'
 // import './03-solid/02-open-close-a'
-import './03-solid/03-liskov-a'
+// import './03-solid/03-liskov-a'
+import './03-solid/05-dependency-a'
 const app = document.querySelector<HTMLDivElement>('#app')!
 
 app.innerHTML = `
